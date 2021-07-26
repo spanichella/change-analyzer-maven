dfee518e1316c01db39d56bf8c6038a4d0e2d793
spanichella <spanichella@gmail.com>
2021-07-26 16:15:13 +0200

    added C# example

---
+++ change-analyzer-maven/resources/cs-class-v1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+    public static int Sum(int num1, int num2)
+    {
+        int total;
+        total = num1 + num2;
+        return total;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, world!");
+        Console.ReadLine();
+    }
+    }
+}
+
